13a0a7b41b5a44cb9eda70b51914e0424204eb0a
jazhang1999 <jazhang1999@gmail.com>
2020-08-17 15:29:48 -0400

    Remove old files

--- testAutoShutdown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Running script to determine if shutdown is necessary"
-exitStatus=$(/usr/bin/python3 /home/nick/src/git/Speedtest/checkLastUpdated.py)
-
-if test $exitStatus == 1; then
-    echo "Shutdown command goes here"   
-fi
